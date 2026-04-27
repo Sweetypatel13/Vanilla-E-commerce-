4abf7f57c8be55eeadde4bc13e144799a1691682
Sweetypatel13 <patelsweety1304@gmail.com>
2026-04-27 10:15:20 +0530

    created and provide script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello!');
+import { products } from "./data.js";
+
+const container = document.getElementById("product-container");
+
+products.forEach(product => {
+  const div = document.createElement("div");
+  div.className = "card";
+
+  const discounted = product.price - (product.price * product.discount / 100);
+
+  div.innerHTML = `
+    <img src="${product.image}">
+    <h3>${product.name}</h3>
+    <p>⭐ ${product.rating}</p>
+    <p>${product.discount}% OFF</p>
+    <p><del>₹${product.price}</del> ₹${discounted}</p>
+    <button>Add to Cart</button><br><br>
+    <button>Buy</button>
+  `;
+
+  container.appendChild(div);
+});
